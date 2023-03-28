4d09348e8e0e0abbad8c00ab028ca1763594e145
Wallace Gregorio <wallacegregorio1987@gmail.com>
2023-03-27 21:41:54 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace CountPDFOnDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\walla\Downloads\01 - recapitulacao-de-logica-de-programacao";
+            int count = Directory.GetFiles(path).Where(File => File.ToLower().EndsWith(".pdf")).Count();
+
+            Console.WriteLine($"Número de arquivos PDF em {path}: {count}");
+        }
+    }
+}
